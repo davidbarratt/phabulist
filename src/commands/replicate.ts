4addc74be894066b8bb8b303e19b1b10361daa3c
David Barratt <david@davidwbarratt.com>
2021-03-27 18:38:41 -0400

    Add replicator command

---
+++ src/commands/replicate.ts
@@ -0,0 +1,295 @@
+import { Command } from '@oclif/command';
+import { Subject, of, defer, from } from 'rxjs';
+import { delay, share, flatMap, switchMap, first } from 'rxjs/operators';
+import fetch, { RequestInfo, RequestInit } from 'node-fetch';
+import * as inquirer from 'inquirer';
+import * as autocomplete from 'inquirer-autocomplete-prompt';
+
+inquirer.registerPrompt('autocomplete', autocomplete);
+
+interface Option {
+  name: string;
+  value: string;
+}
+
+enum PhabricatorObjectType {
+  PROJ = 'PROJ',
+  TASK = 'TASK',
+}
+
+interface PhabricatorTaskPriority {
+  name: string;
+  keywords: string[];
+  short: string;
+  color: string;
+  value: number;
+}
+
+interface PhabricatorTask {
+  id: number;
+  type: PhabricatorObjectType.TASK;
+  phid: string;
+  fields: {
+    name: string;
+    description: {
+      raw: string;
+    };
+    authorPHID: string;
+    ownerPHID: string;
+    status: {
+      value: string;
+      name: string;
+      color: string|null;
+    };
+    priority: {
+      value: number;
+      name: string;
+      color: string;
+    };
+    points: string;
+    subtype: string;
+    closerPHID: string;
+    dateClosed: number;
+    spacePHID: string;
+    dateCreated: string;
+    dateModified: string;
+    policy: {
+      view: string;
+      interact: string;
+      edit: string;
+    };
+  };
+  attachments: {
+    projects?: {
+      projectPHIDs: string[];
+    };
+  };
+}
+
+interface PhabricatorProject {
+  id: number;
+  type: PhabricatorObjectType.PROJ;
+  phid: string;
+  fields: {
+    name: string;
+    slug: string|null;
+    subtype: string;
+    milestone: number;
+    depth: number;
+    parent: {
+      id: number;
+      phid: string;
+      name: string;
+    };
+    icon: {
+      key: string;
+      name: string;
+      icon: string;
+    };
+    color: {
+      key: string;
+      name: string|null;
+    };
+    spacePHID: string;
+    dateCreated: number;
+    dateModified: number;
+    policy: {
+      view: string;
+      edit: string;
+      join: string;
+    };
+    description: string|null;
+  };
+  attachments: Record<string, any>[];
+}
+
+interface PhabricatorResult<T> {
+  data: T;
+}
+
+interface ConduitResponse<T> {
+  result: PhabricatorResult<T>|null;
+  error_code: string|null;
+  error_info: string|null;
+}
+
+export default class Replicate extends Command {
+  static description = 'Duplicate tasks from one project to another';
+
+  private searchSubject = new Subject<string>();
+
+  private searchProject = this.searchSubject.pipe(
+    // Use switch map so the only values that are dropped are the ones that were dropped by the handler already.
+    switchMap(value => {
+      if (!value) {
+        return of([]);
+      }
+
+      return of(value).pipe(
+        // This is still going to execute it... should figure out how to change this to a debounce.
+        delay(200),
+        flatMap(v => {
+          const url = new URL('/api/project.search', process.env.PHABRICATOR_URL);
+          const query = new URLSearchParams();
+          query.set('api.token', process.env.PHABRICATOR_CONDUIT_API_TOKEN || '');
+          query.set('constraints[query]', v);
+          query.set('order', 'relevance');
+
+          return defer(() => this.conduitFetch<PhabricatorProject[]>(url.toString(), {
+            body: query.toString(),
+          })).pipe(
+            flatMap(({ data = [] }) => (
+              of<Option[]>(data.map(({ phid, fields }) => ({
+                name: fields.parent ? `${fields.name} (${fields.parent.name})` : fields.name,
+                value: phid,
+              })))
+            ))
+          );
+        }),
+      );
+    }),
+    // Keep alive.
+    share(),
+  );
+
+  private handleSource(answers: Option[], value: string) {
+    const options = this.searchProject.pipe(first()).toPromise();
+    this.searchSubject.next(value);
+
+    return options;
+  }
+
+  private async conduitFetch<T>(resource: RequestInfo, options: RequestInit | undefined = {}) {
+    const response = await fetch(resource, {
+      method: 'POST',
+      ...options,
+    });
+
+    const conduitResponse = await response.json() as ConduitResponse<T>;
+
+    if (!conduitResponse.result) {
+      throw new Error(conduitResponse.error_info || 'Phabricator API Error');
+    }
+
+    return conduitResponse.result;
+  }
+
+  private async fetchPriorities() {
+    const url = new URL('/api/maniphest.priority.search', process.env.PHABRICATOR_URL);
+    const query = new URLSearchParams();
+    query.set('api.token', process.env.PHABRICATOR_CONDUIT_API_TOKEN || '');
+
+    const result = await this.conduitFetch<PhabricatorTaskPriority[]>(url.toString(), {
+      body: query.toString(),
+    });
+
+    return result.data || [];
+  }
+
+  private async fetchTasks(source: string, tag: string) {
+    const url = new URL('/api/maniphest.search', process.env.PHABRICATOR_URL);
+    const query = new URLSearchParams();
+    query.set('api.token', process.env.PHABRICATOR_CONDUIT_API_TOKEN || '');
+    query.set('constraints[projects][0]', source);
+    query.set('constraints[projects][1]', tag);
+    query.set('constraints[statuses][0]', 'resolved');
+    query.set('attachments[projects]', '1');
+
+    const result = await this.conduitFetch<PhabricatorTask[]>(url.toString(), {
+      body: query.toString(),
+    });
+
+    return result.data || [];
+  }
+
+  async run() {
+    const prioritiesFetch = this.fetchPriorities();
+
+    const { source, tag } = await inquirer.prompt([
+      {
+        type: 'autocomplete',
+        name: 'source',
+        message: 'Source Project',
+        source: this.handleSource.bind(this),
+      },
+      {
+        type: 'autocomplete',
+        name: 'tag',
+        message: 'Tag',
+        source: this.handleSource.bind(this),
+      },
+    ]);
+
+    const tasks = await this.fetchTasks(source, tag);
+
+    if (tasks.length === 0) {
+      this.log('No tasks to copy!');
+      return;
+    }
+
+    const { destination } = await inquirer.prompt([
+      {
+        type: 'autocomplete',
+        name: 'destination',
+        message: 'Destination',
+        source: this.handleSource.bind(this),
+      },
+    ]);
+
+    const { confirm } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'confirm',
+        message: `Copy ${tasks.length} task(s)?`,
+        suffix: '\n' + tasks.map(task => `T${task.id} ${task.fields.name}`).join('\n') + '\n',
+      },
+    ]);
+
+    if (!confirm) {
+      return;
+    }
+
+    // This should be done by now.
+    const priorities = await prioritiesFetch;
+
+    const edits = from(tasks).pipe(
+      flatMap(task => {
+        const { fields, attachments } = task;
+        const url = new URL('/api/maniphest.edit', process.env.PHABRICATOR_URL);
+        const query = new URLSearchParams();
+        query.set('api.token', process.env.PHABRICATOR_CONDUIT_API_TOKEN || '');
+        query.set('transactions[0][type]', 'title');
+        query.set('transactions[0][value]', fields.name);
+        query.set('transactions[1][type]', 'status');
+        query.set('transactions[1][value]', 'open');
+        query.set('transactions[2][type]', 'priority');
+        query.set('transactions[2][value]', priorities.find(p => p.value === fields.priority.value)?.keywords[0] || '');
+        query.set('transactions[3][type]', 'points');
+        query.set('transactions[3][value]', fields.points);
+        query.set('transactions[4][type]', 'description');
+        query.set('transactions[4][value]', fields.description.raw);
+        query.set('transactions[5][type]', 'owner');
+        query.set('transactions[5][value]', fields.ownerPHID);
+
+        query.set('transactions[6][type]', 'projects.set');
+        [
+          ...attachments.projects?.projectPHIDs.filter(phid => phid !== source) || [],
+          destination,
+        ].forEach((phid, i) => {
+          query.set(`transactions[6][value][${i}]`, phid);
+        });
+
+        return defer(() => fetch(url.toString(), {
+          method: 'POST',
+          body: query.toString(),
+        })).pipe(
+          flatMap(response => response.json()),
+        );
+      }, 2),
+    );
+
+    edits.subscribe(({ result: { object: { id } } }) => {
+      this.log(`Created T${id}`);
+    });
+  }
+}
